88819b0691c95bff84fd6a37543036c6a5f339e0
Tim <tim.jungk@edu.fhdw.de>
2024-04-24 13:32:34 +0200

    Add LSP, Lint & Plugins

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "angularls", "dockerls", "docker_compose_language_service", "eslint", "jdtls", "rust_analyzer", "tsserver" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
