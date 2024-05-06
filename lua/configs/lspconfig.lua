3d9333fa18b65ca62801f0806808a0c78f464147
Tim Jungk <tim.jungk@edu.fhdw.de>
2024-05-06 16:11:02 +0200

    Auto Formatter und Deno LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,11 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "angularls", "dockerls", "docker_compose_language_service", "eslint", "jdtls", "rust_analyzer", "tsserver" }
+local servers =
+  { "html", "cssls", "angularls", "dockerls", "docker_compose_language_service", "eslint", "jdtls", "rust_analyzer" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,9 +16,13 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
+lspconfig.denols.setup {
+  on_attach = on_attach,
+  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
 lspconfig.tsserver.setup {
   on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+  root_dir = lspconfig.util.root_pattern "package.json",
+  single_file_support = false,
 }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,39 +1,59 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
   },
 
-   {
-     "neovim/nvim-lspconfig",
-     config = function()
-       require("nvchad.configs.lspconfig").defaults()
-       require "configs.lspconfig"
-     end,
-   },
-  
-   {
-   	"williamboman/mason.nvim",
-   	opts = {
-   		ensure_installed = {
-   			"lua-language-server", "stylua",
-   			"html-lsp", "css-lsp" , "prettier", 
-        "angular-language-server", "docker-compose-language-service",
-        "dockerfile-language-server", "eslint-lsp", "jdtls", "rust-analyzer", "ast-grep"
-   		},
-   	},
-   },
-  
-   {
-   	"nvim-treesitter/nvim-treesitter",
-   	opts = {
-   		ensure_installed = {
-   			"vim", "lua", "vimdoc",
-        "html", "css", "angular", "java", "latex", "rust", "typescript", ""
-   		},
-   	},
-   },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "angular-language-server",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "eslint-lsp",
+        "jdtls",
+        "rust-analyzer",
+        "ast-grep",
+        "deno",
+        "typescript-language-server",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "angular",
+        "java",
+        "rust",
+        "typescript",
+      },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+  },
 }
