682d16e1a0f238f9ca76470287403d1b34d3c145
Tim Jungk <tim.jungk@edu.fhdw.de>
2024-05-24 12:00:24 +0200

    Md

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,4 +56,12 @@ return {
   {
     "sindrets/diffview.nvim",
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
