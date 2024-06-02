6241958121854f95db510c1b4116215eab494b23
Tim Jungk <tim.jungk@edu.fhdw.de>
2024-06-02 15:02:47 +0200

    Add Python LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,8 +4,17 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers =
-  { "html", "cssls", "angularls", "dockerls", "docker_compose_language_service", "eslint", "jdtls", "rust_analyzer" }
+local servers = {
+  "html",
+  "cssls",
+  "angularls",
+  "dockerls",
+  "docker_compose_language_service",
+  "eslint",
+  "jdtls",
+  "rust_analyzer",
+  "pyright",
+}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
